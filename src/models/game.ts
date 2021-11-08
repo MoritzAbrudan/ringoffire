59e1bc921772c4d2e5093c0d7aaafb46ae576c42
MoritzAbrudan <moritzabrudan@hotmail.de>
2021-11-08 15:32:18 +0100

    palyers

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ['Moritz', 'Mela', 'Miri', 'Sophie'];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
